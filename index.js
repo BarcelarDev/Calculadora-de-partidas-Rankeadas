5784043393915166f177af566676ba19317f127e
andre-barcelar@live,com <andre-barcelar@live.com>
2026-03-05 18:42:07 -0300

    Criação da Função

---
+++ index.js
@@ -0,0 +1,6 @@
+// Função que calcula o saldo e determina o nível
+function calcularNivel(vitorias, derrotas) {
+    // Cálculo do saldo
+    let saldoVitorias = vitorias - derrotas;
+    let nivel = "";
+
